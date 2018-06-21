5281b1120394e79edb2210ccd763f644f32cba89
codemasher <smiley@chillerlan.net>
2018-06-21 03:33:16 +0200

    :sparkles:

---
+++ tests/ImagetilerTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Class ImagetilerTest
+ *
+ * @filesource   ImagetilerTest.php
+ * @created      20.06.2018
+ * @package      chillerlan\ImagetilerTest
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2018 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\ImagetilerTest;
+
+use chillerlan\Imagetiler\Imagetiler;
+use chillerlan\Imagetiler\ImagetilerOptions;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass, ReflectionMethod;
+
+/**
+ */
+class ImagetilerTest extends TestCase{
+
+	/**
+	 * @var \ReflectionClass
+	 */
+	protected $reflection;
+
+	protected function setUp(){
+		$this->reflection = new ReflectionClass(Imagetiler::class);
+	}
+
+	public function testGetSize(){
+
+		$max = 22;
+
+		$options = new ImagetilerOptions([
+			'zoom_min'             => 0,
+			'zoom_max'             => $max,
+			'zoom_normalize'       => 4,
+		]);
+
+		$tiler = new Imagetiler($options);
+
+		for($z = 0; $z <= $max; $z++){
+			$v = $this->getMethod('getSize')->invokeArgs($tiler, [4096, 2048, $z]);
+
+			$expected = 2 ** $z * 256;
+
+			$this->assertSame($expected, $v[0]);
+			$this->assertSame($expected/2, $v[1]);
+		}
+
+	}
+
+	/**
+	 * @param string $method
+	 *
+	 * @return \ReflectionMethod
+	 */
+	protected function getMethod(string $method):ReflectionMethod {
+		$method = $this->reflection->getMethod($method);
+		$method->setAccessible(true);
+
+		return $method;
+	}
+
+
+}
