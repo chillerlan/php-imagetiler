71cbb7ae4957bc05fdfdf3871f32e78eea2ffdb7
codemasher <smiley@chillerlan.net>
2018-06-21 22:33:04 +0200

    :octocat: updated example

--- examples/imagetiler.php
+++ examples/imagetiler.php
@@ -12,10 +12,12 @@ namespace chillerlan\ImagetilerExamples;
 use chillerlan\Imagetiler\{Imagetiler, ImagetilerException, ImagetilerOptionsTrait};
 use chillerlan\Logger\{Log, LogOptionsTrait, Output\ConsoleLog};
 use chillerlan\Traits\ContainerAbstract;
+use ImageOptimizer\OptimizerFactory;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $input = __DIR__.'/[YOUR HUGE IMAGE].png';
+$utils = __DIR__.'/../../../utils/%s.exe';
 
 $options = [
 	// ImagetilerOptions
@@ -28,16 +30,27 @@ $options = [
 	'overwrite_base_image' => false,
 	'overwrite_tile_image' => true,
 	'clean_up'             => false,
+	'optimize_output'      => true,
+	'memory_limit'         => '2G',
 	// LogOptions
 	'minLogLevel' => 'debug',
+];
+
+$optimizer_settings = [
+	'execute_only_first_png_optimizer' => false,
+	'advpng_bin'   => sprintf($utils, 'advpng'),
+	'optipng_bin'  => sprintf($utils, 'optipng'),
+	'pngcrush_bin' => sprintf($utils, 'pngcrush'),
+	'pngquant_bin' => sprintf($utils, 'pngquant'),
 ];
 
 $options = new class($options) extends ContainerAbstract{
 	use ImagetilerOptionsTrait, LogOptionsTrait;
 };
 
-$logger = (new Log)->addInstance(new ConsoleLog($options), 'console');
-$tiler  = new Imagetiler($options, $logger);
+$logger    = (new Log)->addInstance(new ConsoleLog($options), 'console');
+$optimizer = (new OptimizerFactory($optimizer_settings, $logger))->get();
+$tiler     = new Imagetiler($options, $optimizer, $logger);
 
 try{
 	$tiler->process($input, __DIR__.'/tiles');
